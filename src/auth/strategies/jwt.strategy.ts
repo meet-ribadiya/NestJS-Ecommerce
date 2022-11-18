2843f7a03fee4c5d59aed0e45618a718ac304113
meet ribadiya <ribadiyameet1@gmail.com>
2022-11-18 10:07:02 +0530

    initial commit

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,24 @@
+import { ExtractJwt } from 'passport-jwt';
+import { Strategy } from 'passport-local';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import 'dotenv/config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(payload: any) {
+    return {
+      userId: payload.sub,
+      username: payload.username,
+      roles: payload.roles,
+    };
+  }
+}
